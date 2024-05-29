0107af640950fbff7f5268f369bd6ad6dcdc0555
Milosz Olejnik <milosz.olejnik@tuta.io>
2024-05-29 19:46:12 +0200

    Create Navbar structure

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,15 +1,47 @@
+'use client'
 export default function Nav(){
     return(
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">Crafty Books</a>
+                    <a className="text-xl font-bold cursor-pointer">Crafty Books</a>
                 </div>
                 <div className="flex-none">
-                    <button className="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
-                        </svg>
-                    </button>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button">
+                            <button className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <ul 
+                        tabIndex={0} 
+                        className="
+                            menu 
+                            dropdown-content 
+                            z-[1] 
+                            p-2 
+                            shadow 
+                            bg-base-100 
+                            rounded-box 
+                            w-52 
+                            mt-4
+                            font-bold
+                            ">
+                            <li>
+                                <h2>Sklep</h2>
+                            </li>
+                            <li>
+                                <h2>O Mnie</h2>
+                            </li>
+                            <li>
+                                <h2>Galeria</h2>
+                            </li>
+                            <li>
+                                <h2>Kontatk</h2>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <button className="btn">Hello body</button>
+        <Hero/>
     </main>
   );
 }
